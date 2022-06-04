3a5920f879fd71b17e91e586b8eed46c9a0508d8
Murilo Cunha <murilo.sant@hotmail.com>
2022-06-04 16:39:40 -0300

    feat(classroom): replicating purchases project setup into classroom project

---
+++ classroom/src/http/test.resolver.ts
@@ -0,0 +1,15 @@
+import { UseGuards } from '@nestjs/common';
+import { Int, Query, Resolver } from '@nestjs/graphql';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+
+@Resolver()
+export class TestResolver {
+  constructor(private prisma: PrismaService) {}
+
+  @Query(() => Int)
+  @UseGuards(AuthorizationGuard) // middleware
+  hello() {
+    return 100;
+  }
+}
