fd32482ac46d13cacf56038e856cf736cf012b20
Murilo Cunha <murilo.sant@hotmail.com>
2022-06-04 12:22:27 -0300

    feat(purchases): initial auth0 setup

---
+++ purchases/src/http/auth/authorization.guard.ts
@@ -0,0 +1,59 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import jwt from 'express-jwt';
+import { expressJwtSecret } from 'jwks-rsa';
+import { promisify } from 'node:util';
+
+// promisify => converte uma função que usa padrão async de callbacks, pra Promises
+
+// Middleware (Express) (request, response)
+
+@Injectable()
+export class AuthorizationGuard implements CanActivate {
+  private AUTH0_AUDIENCE: string;
+  private AUTH0_DOMAIN: string;
+
+  /**
+   * O sistema vai injetar dentro dessa class o configService automaticamente.
+   * Por isso o @Injectable.
+   * Utilizando conceito no SOLID de inversão de dependência e injeção de dependência.
+   * @param configService configurações do nestjs
+   */
+  constructor(private configService: ConfigService) {
+    this.AUTH0_AUDIENCE = this.configService.get('AUTH0_AUDIENCE') ?? ''; // dentro do get precisa ser o mesmo nome da env
+    this.AUTH0_DOMAIN = this.configService.get('AUTH0_DOMAIN') ?? '';
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const httpContext = context.switchToHttp();
+    const req = httpContext.getRequest();
+    const res = httpContext.getResponse();
+
+    const checkJWT = promisify(
+      jwt({
+        secret: expressJwtSecret({
+          cache: true,
+          rateLimit: true,
+          jwksRequestsPerMinute: 5,
+          jwksUri: `${this.AUTH0_DOMAIN}.well-known/jwks.json`,
+        }),
+        audience: this.AUTH0_AUDIENCE,
+        issuer: this.AUTH0_DOMAIN,
+        algorithms: ['RS256'],
+      }),
+    );
+
+    try {
+      await checkJWT(req, res);
+
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException(error);
+    }
+  }
+}
