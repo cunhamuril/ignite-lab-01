f1e65ebdada73b691a3c5c1b86be14b5cf361568
Murilo Cunha <murilo.sant@hotmail.com>
2022-06-07 20:43:41 -0300

    feat(purchases): create list all products query

---
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -0,0 +1,18 @@
+// import { UseGuards } from '@nestjs/common';
+import { Query, Resolver } from '@nestjs/graphql';
+
+// import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+import { ProductsService } from 'src/services/products.service';
+
+import { Product } from '../models/product';
+
+@Resolver()
+export class ProductsResolver {
+  constructor(private productsService: ProductsService) {}
+
+  @Query(() => [Product])
+  // @UseGuards(AuthorizationGuard) // middleware
+  products() {
+    return this.productsService.listAllProducts();
+  }
+}
