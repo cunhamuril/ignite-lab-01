faafa8b475b49d4ed2f58929c467ccf8b107760f
Murilo Cunha <murilo.sant@hotmail.com>
2022-06-08 18:21:25 -0300

    feat(classroom): created enrollments query

--- classroom/src/services/enrollments.service.ts
+++ classroom/src/services/enrollments.service.ts
@@ -5,4 +5,27 @@ import { PrismaService } from 'src/database/prisma/prisma.service';
 @Injectable()
 export class EnrollmentsService {
   constructor(private prisma: PrismaService) {}
+
+  listAllEnrollments() {
+    return this.prisma.enrollment.findMany({
+      where: {
+        canceledAt: null,
+      },
+      orderBy: {
+        createdAt: 'desc', // most recently first
+      },
+    });
+  }
+
+  listEnrollmentsByStudent(studentId: string) {
+    return this.prisma.enrollment.findMany({
+      where: {
+        studentId,
+        canceledAt: null,
+      },
+      orderBy: {
+        createdAt: 'desc', // most recently first
+      },
+    });
+  }
 }
