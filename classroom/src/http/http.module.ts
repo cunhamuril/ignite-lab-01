7077a816ee1400228dbc66be7953c3ccca50c4b1
Murilo Cunha <murilo.sant@hotmail.com>
2022-06-08 17:42:47 -0300

    feat(classroom): inital services setup

--- classroom/src/http/http.module.ts
+++ classroom/src/http/http.module.ts
@@ -4,6 +4,10 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 
+import { CoursesService } from 'src/services/courses.service';
+import { EnrollmentsService } from 'src/services/enrollments.service';
+import { StudentsService } from 'src/services/students.service';
+
 import { DatabaseModule } from '../database/database.module';
 import { CoursesResolver } from './graphql/resolvers/courses.resolver';
 import { EnrollmentsResolver } from './graphql/resolvers/enrollments.resolver';
@@ -23,6 +27,11 @@ import { StudentsResolver } from './graphql/resolvers/students.resolver';
     CoursesResolver,
     EnrollmentsResolver,
     StudentsResolver,
+
+    // Services
+    CoursesService,
+    EnrollmentsService,
+    StudentsService,
   ],
 })
 export class HttpModule {}
