2a1cb3e24888ff64ef0ea6fe87e1385eeaf39f79
Murilo Cunha <murilo.sant@hotmail.com>
2022-06-08 17:58:08 -0300

    feat(classroom): created list all students query

--- classroom/src/services/students.service.ts
+++ classroom/src/services/students.service.ts
@@ -5,4 +5,8 @@ import { PrismaService } from 'src/database/prisma/prisma.service';
 @Injectable()
 export class StudentsService {
   constructor(private prisma: PrismaService) {}
+
+  listAllStudents() {
+    return this.prisma.student.findMany();
+  }
 }
