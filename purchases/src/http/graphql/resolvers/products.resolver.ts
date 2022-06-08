9064a6690fde5f340dccc6e4281b4f0d156c8b00
Murilo Cunha <murilo.sant@hotmail.com>
2022-06-07 21:02:46 -0300

    feat(purchases): added mutation to create products

--- purchases/src/http/graphql/resolvers/products.resolver.ts
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -1,8 +1,9 @@
 // import { UseGuards } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 
 // import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
 import { ProductsService } from 'src/services/products.service';
+import { CreateProductInput } from '../inputs/create-product-input';
 
 import { Product } from '../models/product';
 
@@ -14,5 +15,10 @@ export class ProductsResolver {
   // @UseGuards(AuthorizationGuard) // middleware
   products() {
     return this.productsService.listAllProducts();
+  }
+
+  @Mutation(() => Product)
+  createProduct(@Args('data') data: CreateProductInput) {
+    return this.productsService.createProduct(data);
   }
 }
