e3f91b91c69e81633db01be8e03e3061f926c440
Murilo Cunha <murilo.sant@hotmail.com>
2022-06-08 17:32:39 -0300

    feat(classroom): initial graphQL models setup

--- classroom/src/http/http.module.ts
+++ classroom/src/http/http.module.ts
@@ -5,7 +5,6 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 
 import { DatabaseModule } from '../database/database.module';
-import { TestResolver } from './test.resolver';
 
 @Module({
   imports: [
@@ -16,6 +15,6 @@ import { TestResolver } from './test.resolver';
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],
-  providers: [TestResolver],
+  providers: [],
 })
 export class HttpModule {}
