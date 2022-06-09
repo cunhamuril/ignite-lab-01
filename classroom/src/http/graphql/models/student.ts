7ca67df7e02cfa1e3e1e2c9e016a0e3a076eea94
Murilo Cunha <murilo.sant@hotmail.com>
2022-06-09 19:45:31 -0300

    feat(classroom): tweaks for API gateway integration

--- classroom/src/http/graphql/models/student.ts
+++ classroom/src/http/graphql/models/student.ts
@@ -1,10 +1,15 @@
-import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { Directive, Field, ID, ObjectType } from '@nestjs/graphql';
 import { Enrollment } from './enrollment';
 
-@ObjectType()
+@ObjectType('User') // nome em comum para student e customer
+@Directive('@extends')
+@Directive('@key(fields: "authUserId")')
 export class Student {
-  @Field(() => ID)
   id: string;
+
+  @Field(() => ID)
+  @Directive('@external')
+  authUserId: string;
 
   @Field(() => [Enrollment])
   enrollments: Enrollment[];
